827cb685104a508500ddb64fa4aefda82f0e6d76
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-09-20 20:58:00 -0300

    Feat: Requirement 2 complete

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,13 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    if not path_file.endswith(".txt"):
+        print("Formato inválido", file=sys.stderr)
+
+    try:
+        with open(path_file, mode="r") as file:
+            reading = file.read()
+            return reading.splitlines()
+    except Exception:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
