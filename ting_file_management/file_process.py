c440448cd7776549e53661023c3f944db07c69e7
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-09-20 22:34:30 -0300

    Feat: Requirements 3, 4, 5 complete

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,10 +1,35 @@
+import sys
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    for file in instance.data:
+        if file["nome_do_arquivo"] is path_file:
+            return None
+
+    output_structure = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(txt_importer(path_file)),
+        "linhas_do_arquivo": txt_importer(path_file)
+    }
+
+    instance.enqueue(output_structure)
+    print(output_structure, file=sys.stdout)
 
 
 def remove(instance):
-    """Aqui irá sua implementação"""
+    if not instance:
+        return print("Não há elementos", file=sys.stdout)
+
+    first_file_remove = instance.dequeue()
+    print(
+        f"Arquivo {first_file_remove['nome_do_arquivo']} removido com sucesso",
+        file=sys.stdout
+    )
 
 
 def file_metadata(instance, position):
-    """Aqui irá sua implementação"""
+    try:
+        print(instance.search(position), file=sys.stdout)
+    except IndexError:
+        print("Posição inválida", file=sys.stderr)
