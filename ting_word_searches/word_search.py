3b135c306eea9060109de81ddf7e68c942001a4d
Bruno Ribeiro <ribeiro.brunocsr@gmail.com>
2022-09-20 23:35:53 -0300

    Feat: Requirements 6, 7 complete

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,44 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    exist = list()
+
+    for i in instance.data:
+        occurrences = list()
+
+        for words, rows in enumerate(i['linhas_do_arquivo']):
+            if word.lower() in rows.lower():
+                occurrences.append({"linha": words + 1})
+
+        if len(occurrences) > 0:
+            exist.append(
+                {
+                    "palavra": word,
+                    "arquivo": i['nome_do_arquivo'],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return exist
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    exist = list()
+
+    for i in instance.data:
+        occurrences = list()
+
+        for words, rows in enumerate(i['linhas_do_arquivo']):
+            if word.lower() in rows.lower():
+                occurrences.append(
+                    {"linha": words + 1, "conteudo": rows}
+                )
+
+        if len(occurrences) > 0:
+            exist.append(
+                {
+                    "palavra": word,
+                    "arquivo": i['nome_do_arquivo'],
+                    "ocorrencias": occurrences,
+                }
+            )
+
+    return exist
